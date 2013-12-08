a29c6a89e50819e764bfc36c6308a581008f596b
yaomuyang <yaomuyang1992@gmail.com>
2013-12-08 16:39:00 +0800

    added a monitor to gui, to trace the possible commands

--- DreamHalma/src/cn/edu/tsinghua/ee/Dreamland/DreamHalma/Backend.java
+++ DreamHalma/src/cn/edu/tsinghua/ee/Dreamland/DreamHalma/Backend.java
@@ -4,6 +4,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.ObjectOutputStream;
 import java.io.BufferedOutputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -45,6 +47,13 @@ public class Backend implements Runnable{
 				System.exit(1);
 			}
 			LOG.info("Backend Initiated");
+			try{
+				this.startGame();
+			} catch (Exception e){
+				LOG.error("game halted in error: "+e.getStackTrace());
+				System.exit(1);
+			}
+			LOG.info("Game finished successfully");
 		}
 		else {
 			LOG.error("failed to find if its client or server");
@@ -52,14 +61,35 @@ public class Backend implements Runnable{
 		}
 	}
 	
+	private void startGame() throws Exception{
+		boolean finished = false;
+		int serverPortMovement = Integer.parseInt(configure.getProperty("backend_port_movement"));
+		ServerSocket server;
+		server = new ServerSocket(serverPortMovement);
+		while (!finished){
+			Socket socket = server.accept();
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String Command = br.readLine();
+			ObjectOutputStream oos = new ObjectOutputStream(
+					new BufferedOutputStream(socket.getOutputStream()));
+			Message message = new Message(state);
+			oos.writeObject(message);
+			oos.flush();
+			br.close();
+			oos.close();
+			socket.close();
+		}
+		server.close();
+	}
+	
 	//a thread to receive and send out heartbeat information
 	private class HeartBeat implements Runnable{
 		public void run(){
 			LOG.info("Heartbeat Server Initiating");
-			int serverPort = Integer.parseInt(configure.getProperty("backend_port_heartbeat"));
+			int serverPortHeartBeat = Integer.parseInt(configure.getProperty("backend_port_heartbeat"));
 			ServerSocket server;
 			try{
-				server = new ServerSocket(serverPort);	
+				server = new ServerSocket(serverPortHeartBeat);	
 				while(true){
 					Socket socket = server.accept();
 					ObjectOutputStream oos = new ObjectOutputStream(
